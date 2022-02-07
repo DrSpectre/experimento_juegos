acb4f6483501e173b12659b5ea9bbdb8afd2f767
Azxma <raccoonevef@gmail.com>
2022-02-06 19:18:08 -0700

    [MOD] Modifica el comportamiento de la puerta para moverse de dos maneras y agrega control de veocidades negativas

--- Objetos/objetos_interactuables/Puerta.gd
+++ Objetos/objetos_interactuables/Puerta.gd
@@ -1,19 +1,34 @@
 extends RigidBody2D
 
+enum tipos_puerta {
+	tipo_rotativa,
+	tipo_traslacion
+}
 
 onready var sistema_control = $controlador
-export(float) var velocidad = 1.5
+export(float) var velocidad = -1.5
 
 export(bool) var abierta = false
+export(tipos_puerta) var tipo_de_puerta = tipos_puerta.tipo_traslacion
+export(float) var tope = 30.0
+
 export(Array, NodePath) var controladores_exteriores
 
+var mem_data = null
 
 func _ready():
-	if not abierta:
-		rotation_degrees = 0
+	if tipo_de_puerta == tipos_puerta.tipo_rotativa:
+		mem_data = rotation_degrees
 	
-	else:
-		rotation_degrees = 90
+	elif tipo_de_puerta == tipos_puerta.tipo_traslacion:
+		mem_data = position
+	
+	if abierta:
+		if tipo_de_puerta == tipos_puerta.tipo_rotativa:
+			rotation_degrees = rotation_degrees + tope
+	
+		elif tipo_de_puerta == tipos_puerta.tipo_traslacion:
+			position.x = position.x + tope
 
 	set_physics_process(false)
 	
@@ -24,14 +39,40 @@ func _ready():
 		get_node(unl).connect("iniciar_proceso", self, "interaccion")
 
 func _physics_process(delta):
-	if abierta:
-		if rotation_degrees >= 90:
+	if abierta and tipo_de_puerta == tipos_puerta.tipo_rotativa:
+		# if rotation_degrees >= mem_data + tope:
+		if abs(rotation_degrees - mem_data) >= tope:
 			abierta = true
 			set_physics_process(false)
-
+			
 		else:
 			rotation_degrees = rotation_degrees + velocidad
+			
+	elif abierta and tipo_de_puerta == tipos_puerta.tipo_traslacion:
+		if abs(position.x - mem_data.x) >= tope:
+			abierta = true
+			set_physics_process(false)
+			
+		else:
+			position.x = position.x + velocidad
 
+	elif not abierta and tipo_de_puerta == tipos_puerta.tipo_rotativa:
+		if abs(rotation_degrees - mem_data) <= 0:
+			abierta = false
+			set_physics_process(false)
+		
+		else:
+			rotation_degrees = rotation_degrees - velocidad
+		
+	elif not abierta and tipo_de_puerta == tipos_puerta.tipo_traslacion:
+		if abs(position.x - mem_data.x) <= 0:
+			abierta = false
+			set_physics_process(false)
+		
+		else:
+			position.x = position.x - velocidad
+
+"""
 	else:
 		if rotation_degrees <= 0:
 			abierta = false
@@ -39,7 +80,7 @@ func _physics_process(delta):
 
 		else:
 			rotation_degrees = rotation_degrees - velocidad
-
+"""
 func interaccion():
 	set_physics_process(true)
 	abierta = not abierta
