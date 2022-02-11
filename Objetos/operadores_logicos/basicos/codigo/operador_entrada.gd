b90829af24b125407925e2781905fe961cc00308
Azxma <raccoonevef@gmail.com>
2022-02-11 15:42:29 -0700

    [MOD, ADD] Modifica funcionamiento para evitar realizar conexion manual con las consolas o zonas de interaccion. Agrega control y corrige para encadenar sistemas de entrada

--- Objetos/operadores_logicos/basicos/codigo/operador_entrada.gd
+++ Objetos/operadores_logicos/basicos/codigo/operador_entrada.gd
@@ -1,35 +1,44 @@
 extends "res://codigo_generico/operadores_logicos/base_operador_logico.gd"
 
+signal iniciar_proceso()
 
-export(NodePath) var conexion_fisica
 export(bool) var trampa = false
 
 var activado: bool = false
 
 func _ready():
-	if not conexion_fisica:
-		printerr("Error: Enlace no proporcionado en (" + name + ")")
-		return
+	var hijos = get_children()
 	
-	var enlace = get_node(conexion_fisica)
+	for hijo in hijos:
+		if hijo.is_in_group("accionador"):
+			hijo.connect("iniciar_proceso", self, "_logica_entrada")
+		
+		elif hijo.is_in_group("mostrar_estatus"):
+			hijo.conectar(self, "iniciar_proceso")
 	
-	if enlace.is_in_group("accionador"):
-		enlace.connect("iniciar_proceso", self, "_logica_entrada")
-
 	if _hijos.size() > 1:
 		printerr("Demasiados nodos interconectados, usar un operador logico")
 	
-	elif _hijos.size() == 0:
+	elif not _hijos:
 		activado = true
 
 func _logica_interna(entrada):
 	if negado:
 		estado = not estado
 	
-	activado = entrada\
+	if entrada == false:
+		if estado and activado:
+			estado = false
+			activado = false
+			actualizar_estado()
+		return
+	
+	activado = entrada
 	
 	if negado:
 		estado = not estado
+	
+	emit_signal("iniciar_proceso", estado)
 
 func _logica_entrada():
 	if negado:
@@ -38,7 +47,7 @@ func _logica_entrada():
 	if trampa and activado:
 		return
 	
-	if not activado:
+	if _hijos and not _hijos[0].estado:
 		return
 	
 	estado = not estado
@@ -48,5 +57,6 @@ func _logica_entrada():
 	
 	actualizar_estado()
 
-func _actualizar():
-	pass
+func actualizar_estado():
+	.actualizar_estado()
+	emit_signal("iniciar_proceso", estado)
