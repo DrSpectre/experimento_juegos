f2f7ee08babb529a23c6871a6ed22e2f795e4727
Azxma <raccoonevef@gmail.com>
2022-02-11 15:33:14 -0700

    [ADD] Agrega control sobre si el objeto a interactuar es posibble hacer con tocarlo o con posarse y pulsar un boton en especifico

--- Objetos/objetos_interactuables/indicador_interaccion.gd
+++ Objetos/objetos_interactuables/indicador_interaccion.gd
@@ -1,1 +1,14 @@
 extends "res://codigo_generico/controlador_objetos_interactuables.gd"
+
+
+func _on_indicador_interaccion_area_entered(area):
+	if not recogible:
+		return
+	
+	var objetivo = area.get_owner()
+	var estado_espacio = get_world_2d().direct_space_state
+
+	var res = estado_espacio.intersect_ray(global_position, objetivo.global_position, [self, area], objetivo.collision_layer, true, true)
+	
+	if res.collider == objetivo:
+		_en_interaccion_recogible(area)
--- codigo_generico/controlador_objetos_interactuables.gd
+++ codigo_generico/controlador_objetos_interactuables.gd
@@ -3,13 +3,21 @@ extends Node2D
 signal iniciar_proceso()
 
 
+export(bool) var recogible = false
 export(String) var llave = ""
 
 func _ready():
 	set_process(false)
 	set_physics_process(false)
 
+func _en_interaccion_recogible(area):
+	if recogible:
+		emit_signal("iniciar_proceso")
+
 func iniciar_interaccion(llaves_proveidas = [""]):
+	if recogible:
+		return
+	
 	if _verificar_llaves(llaves_proveidas):
 		emit_signal("iniciar_proceso")
 		return true
@@ -22,3 +30,5 @@ func _verificar_llaves(llaves):
 		return true
 	
 	return false
+
+
